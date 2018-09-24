0df741775d6e9ff43df3ea30b411e5c67fb0a302
WinFutureX <kelseyboey@hotmail.com>
2018-09-25 00:19:07 +0800

    Added backstroke

--- plagueis.c
+++ plagueis.c
@@ -1,19 +1,16 @@
-/* PLAGUEIS.C - a total r/prequelmemes disaster       */
-/******************************************************/
-/* "did you heard of the tragedy that reach the man?" */
-/*                  -D the speaker, 2005 or something */
-/* "you are already at full cock now"                 */
-/*                  -allah gold, that same year       */
-/*                                                    */
-/* brought to you by STAR WAR the third gathers:      */
-/* BACKSTROKE OF THE WEST                             */
-/******************************************************/
+/*****************************************************************/
+/*                                                               */
+/* WinFutureX presents - Plagueis (or Reach the Man) version 1.1 */
+/*                                                               */
+/*****************************************************************/
+
 #include <stdio.h>
 
 int main()
 {
+	printf ("\nWinFutureX PRESENTS - PLAGUEIS / REACH THE MAN v1.1\n\n");
 	printf("Did you ever hear the tragedy of Darth Plagueis the wise?\n");
-	printf("(Please answer with 'Y' or 'N'.)\n");
+	printf ("(Please answer with 'Y', 'N' or 'B'.)\n");
 	char answer;
 	scanf("%c", &answer);
 	if (answer == 'Y' || answer == 'y')
@@ -31,6 +28,19 @@ int main()
 		printf("losing his power, which eventually, of course, he did. Unfortunately, he taught\n");
 		printf("his apprentice everything he knew, then his apprentice killed him in his sleep.\n");
 		printf("Ironic. He could save others from death, but not himself.\n\n");
+	}
+	else if (answer == 'B' || answer == 'b')
+	{
+		printf ("\nDid you heard of the tragedy that reach the man? I think to be too is not.\n");
+		printf ("I think that the hopeless situation elder also can't. That is a legend.\n");
+		printf ("Reaching the man cloth space is the emperor. He is so strong and big he even\n");
+		printf ("use the original dint to create life. He is very deep to black influence\n");
+		printf ("understanding. The black influence can the matter that many supermans. But\n");
+		printf ("other people thinks that these are what can't attains. He become more and\n");
+		printf ("more strong and big. Lost his power afterwards afterwards he died. In fact he\n");
+		printf ("teaches own the whole skills all to disciple. Then his land killed him to let\n");
+		printf ("him going to bed. Satirizing the meaning is he can let other people, but is\n");
+		printf ("incapable for dint to the oneself.\n\n");
 	}
 	else
 	{
